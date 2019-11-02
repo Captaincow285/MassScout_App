fbb29f3b3e81b1847bfe034565536efbe0de0781
Captaincow285 <Captaincow285@gmail.com>
2019-11-01 22:20:29 -0400

    A working initial start page. Also, added a logo. Nothing is done!

--- MassScout_App/src/main.java
+++ MassScout_App/src/main.java
@@ -2,13 +2,40 @@ import org.apache.poi.ss.*;
 import org.apache.poi.hssf.*;
 import javafx.*;
 import javax.swing.*;
-
+import java.awt.*;
 @SuppressWarnings("unused")
+
 public class main {
 
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		System.out.println("Test");
+		
+		//Create Window (Using absolute layout)
+		JFrame appSelection = new JFrame();
+		appSelection.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		appSelection.setSize(600, 300);
+		appSelection.getContentPane().setLayout(null);
+		appSelection.setLocationRelativeTo(null);
+		
+		//Application Icon
+		ImageIcon icon = new ImageIcon("MassScout_App/img/Logo.png");
+		//appSelection.setIconImage(icon);
+		
+		//Dropdown menu creation
+		String[] dropdownText = {"Nothing Selected", "Enter a match into the database", "Create a new Spreadsheet", "Load Spreadsheet", "Analyze Data (WIP)", "Exit Application"};
+		JComboBox option1 = new JComboBox(dropdownText);
+		option1.setBounds(190, 120, 220, 40);
+		appSelection.getContentPane().add(option1);
+		
+		//Submit Button
+		JButton submit = new JButton("Submit");
+		submit.setBounds(250, 180, 100, 30);
+		appSelection.getContentPane().add(submit);
+		
+		//Final validate and set Visible
+		appSelection.setVisible(true);
+		
+		//Check for button press
+		
 	}
-
 }
