92ad6002734bb435311121f18e41ffe567c763f7
Captaincow285 <Captaincow285@gmail.com>
2019-11-07 11:45:35 -0500

    Added an Action for an Action listener, did some other things. I'm not sure what too much of this does.

---
+++ MassScout_App/src/SubmitAction.java
@@ -0,0 +1,17 @@
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+public class SubmitAction extends AbstractAction
+{
+	public SubmitAction(String text)
+	{
+		super(text);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent plhdr) 
+	{
+		
+		
+	}
+}
--- MassScout_App/src/main.java
+++ MassScout_App/src/main.java
@@ -18,8 +18,13 @@ public class main {
 		appSelection.setLocationRelativeTo(null);
 		
 		//Application Icon
-		ImageIcon icon = new ImageIcon("MassScout_App/img/Logo.png");
-		//appSelection.setIconImage(icon);
+		ImageIcon logo = new ImageIcon("C:\\Users\\Captaincow285\\git\\MassScout_App\\MassScout_App\\img\\Logo.png");
+		appSelection.setIconImage(logo.getImage());
+		
+		//Sets image above dropdown
+		JLabel picLogo = new JLabel((Icon) logo);
+		picLogo.setBounds(250, 10, 100, 100);
+		appSelection.getContentPane().add(picLogo);
 		
 		//Dropdown menu creation
 		String[] dropdownText = {"Nothing Selected", "Enter a match into the database", "Create a new Spreadsheet", "Load Spreadsheet", "Analyze Data (WIP)", "Exit Application"};
@@ -27,15 +32,19 @@ public class main {
 		option1.setBounds(190, 120, 220, 40);
 		appSelection.getContentPane().add(option1);
 		
+		//Action...?
+		SubmitAction plhdr = new SubmitAction("Submit");
+		
 		//Submit Button
-		JButton submit = new JButton("Submit");
-		submit.setBounds(250, 180, 100, 30);
-		appSelection.getContentPane().add(submit);
+		JButton submitB = new JButton("Submit");
+		submitB.setBounds(250, 180, 100, 30);
+		appSelection.getContentPane().add(submitB);
+		
+		//Action Listener
+		submitB.addActionListener(plhdr);
 		
 		//Final validate and set Visible
 		appSelection.setVisible(true);
-		
-		//Check for button press
 		
 	}
 }
